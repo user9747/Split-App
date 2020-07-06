7d369ae16ef25a217380f024214edcc3caa4aefc
user9747 <padinju@gmail.com>
2020-07-07 04:43:34 +0530

    build files added

---
+++ build/precache-manifest.ae71176f677e93de12e02a504511af63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f181baafb1d8fa2bb387dd70cfe27f22",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7e0b654e9a4e4b3241a",
+    "url": "/static/css/main.0b39b2fc.chunk.css"
+  },
+  {
+    "revision": "3e50724b9ba5f80fca34",
+    "url": "/static/js/2.582f0c3b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.582f0c3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7e0b654e9a4e4b3241a",
+    "url": "/static/js/main.0f7153e2.chunk.js"
+  },
+  {
+    "revision": "e76c7c8817f76d39b27a",
+    "url": "/static/js/runtime-main.cb0ca66e.js"
+  }
+]);
